6dc57967f5f27c3d1536167c54df20f0aeb0cedc
Nikin Baidar <nikinbaidarr@gmail.com>
2022-01-14 15:58:42 +0545

    synchronize /home/nikin/.dotfiles/

--- config/cmus/lib.pl
+++ config/cmus/lib.pl
@@ -1,32 +1,95 @@
+/home/nikin/Music/Jati Maya Laye Pani.mp3
+/home/nikin/Music/Kryptonite.mp3
+/home/nikin/Music/Here Without You.mp3
 /home/nikin/Music/Someone Like You.mp3
+/home/nikin/Music/Rolling in the Deep.mp3
+/home/nikin/Music/Ma Ra Malai.mp3
+/home/nikin/Music/Bachau.mp3
+/home/nikin/Music/Nischal.mp3
+/home/nikin/Music/Abhiman.mp3
+/home/nikin/Music/Chaina.mp3
+/home/nikin/Music/Jhari Ko Raat.mp3
 /home/nikin/Music/Timi Bhane.mp3
 /home/nikin/Music/Awaz.mp3
 /home/nikin/Music/Khaseka Tara.mp3
-/home/nikin/Music/Ma Ra Malai.mp3
-/home/nikin/Music/Bachau.mp3
+/home/nikin/Music/Koshish.mp3
+/home/nikin/Music/Sacred.mp3
 /home/nikin/Music/All My Tears.mp3
 /home/nikin/Music/Do I Wanna Know.mp3
+/home/nikin/Music/Why'd You Only Call Me When You're High.mp3
+/home/nikin/Music/Timro Nai Maya (Cover).mp3
+/home/nikin/Music/Wake Me UP.mp3
+/home/nikin/Music/Waiting For Love.mp3
+/home/nikin/Music/Complicated.mp3
 /home/nikin/Music/Mari Jau.mp3
-/home/nikin/Music/Iron Man.mp3
+/home/nikin/Music/Kewal.mp3
 /home/nikin/Music/War Pigs.mp3
+/home/nikin/Music/Paranoid.mp3
+/home/nikin/Music/Iron Man.mp3
+/home/nikin/Music/End Of The Beginning.mp3
+/home/nikin/Music/Loner.mp3
+/home/nikin/Music/She's Gone.mp3
 /home/nikin/Music/Bed of Roses.mp3
+/home/nikin/Music/Livin' On A Prayer.mp3
+/home/nikin/Music/Always.mp3
+/home/nikin/Music/It's My Life.mp3
 /home/nikin/Music/P.O.W..mp3
 /home/nikin/Music/Tears Don't Fall.mp3
 /home/nikin/Music/Your Betrayal.mp3
+/home/nikin/Music/Hearts Burst Into Fire.mp3
+/home/nikin/Music/All These Things I Hate.mp3
+/home/nikin/Music/A Place Where You Belong.mp3
+/home/nikin/Music/Bittersweet Memories.mp3
+/home/nikin/Music/Forever And Always.mp3
+/home/nikin/Music/Tears Don't Fall (Part 2).mp3
+/home/nikin/Music/Ever Fallen In Love.mp3
+/home/nikin/Music/Burn It Down (Acapella).mp3
+/home/nikin/Music/Leave Out All The Rest (Acapella).mp3
+/home/nikin/Music/I'll Be Gone (Acapella).mp3
+/home/nikin/Music/New Divide (Acapella).mp3
+/home/nikin/Music/Numb (Acapella).mp3
+/home/nikin/Music/Powerless (Acapella).mp3
 /home/nikin/Music/Rolling In The Deep Cover.mp3
+/home/nikin/Music/Timro Maya.mp3
+/home/nikin/Music/Mercedes Benz.mp3
+/home/nikin/Music/Fix You.mp3
+/home/nikin/Music/The Scientist.mp3
 /home/nikin/Music/Wicked Game.mp3
+/home/nikin/Music/Snuff (Acoustic).mp3
 /home/nikin/Music/Hesitate.mp3
+/home/nikin/Music/Dying (Acoustic).mp3
+/home/nikin/Music/Home & Zzyxz Road.mp3
+/home/nikin/Music/Tired (Live).mp3
+/home/nikin/Music/Bother (Live).mp3
+/home/nikin/Music/Taciturn (Acoustic).mp3
+/home/nikin/Music/Corey Taylor - Hard Luck Woman-u62_pExenuk.mp3
+/home/nikin/Music/Take It Easy (Cover).mp3
 /home/nikin/Music/Hallowed Be Thy Name Cover.mp3
 /home/nikin/Music/Passenger.mp3
 /home/nikin/Music/Hotel California.mp3
+/home/nikin/Music/Take It Easy.mp3
+/home/nikin/Music/Thinking Out Loud.mp3
+/home/nikin/Music/Dive.mp3
 /home/nikin/Music/Everlong.mp3
 /home/nikin/Music/These Days.mp3
-/home/nikin/Music/Best Of You.mp3
+/home/nikin/Music/Best of You.mp3
 /home/nikin/Music/My Hero.mp3
 /home/nikin/Music/Times Like These.mp3
 /home/nikin/Music/Learn To Fly.mp3
-/home/nikin/Music/Foo Fighters - Best Of You (Official Music Video)-h_L4Rixya64.mp3
+/home/nikin/Music/The Pretender.mp3
 /home/nikin/Music/Carry On.mp3
+/home/nikin/Music/We Are Young.mp3
+/home/nikin/Music/Some Nights.mp3
+/home/nikin/Music/Good Riddance (Time of Your Life).mp3
+/home/nikin/Music/When I Come Around.mp3
+/home/nikin/Music/Revolution Radio.mp3
+/home/nikin/Music/Still Breathing.mp3
+/home/nikin/Music/Stray Heart.mp3
+/home/nikin/Music/Too Dumb to Die.mp3
+/home/nikin/Music/Kill The DJ 8.mp3
+/home/nikin/Music/Basket Case.mp3
+/home/nikin/Music/Hitchin' A Ride.mp3
+/home/nikin/Music/Pulling Teeth.mp3
 /home/nikin/Music/American Idiot.mp3
 /home/nikin/Music/Jesus Of Suburbia.mp3
 /home/nikin/Music/Holiday Boulevard of Broken Dreams.mp3
@@ -36,8 +99,8 @@
 /home/nikin/Music/Wake Me Up When September Ends.mp3
 /home/nikin/Music/Homecoming.mp3
 /home/nikin/Music/Whatsername.mp3
-/home/nikin/Music/Song Of The Century .mp3
-/home/nikin/Music/21st Century Breakdown .mp3
+/home/nikin/Music/Song Of The Century.mp3
+/home/nikin/Music/21st Century Breakdown.mp3
 /home/nikin/Music/Know Your Enemy.mp3
 /home/nikin/Music/¡Viva La Gloria!.mp3
 /home/nikin/Music/Before The Lobotomy.mp3
@@ -60,27 +123,65 @@
 /home/nikin/Music/Don't Cry.mp3
 /home/nikin/Music/Patience.mp3
 /home/nikin/Music/Welcome To The Jungle.mp3
+/home/nikin/Music/Sign of the Times.mp3
+/home/nikin/Music/Kiwi.mp3
+/home/nikin/Music/Two Ghosts.mp3
+/home/nikin/Music/Falling.mp3
+/home/nikin/Music/Take Me To Church.mp3
+/home/nikin/Music/From Eden.mp3
 /home/nikin/Music/Cult Of Personality.mp3
 /home/nikin/Music/Demons.mp3
+/home/nikin/Music/Radioactive.mp3
 /home/nikin/Music/Hallowed Be Thy Name.mp3
+/home/nikin/Music/Fear of the Dark.mp3
 /home/nikin/Music/Dance of Death.mp3
+/home/nikin/Music/2 Minutes To Midnight.mp3
+/home/nikin/Music/Alexander The Great.mp3
+/home/nikin/Music/Phantom of The Opera.mp3
+/home/nikin/Music/The Red And The Black.mp3
 /home/nikin/Music/Blood Brothers.mp3
+/home/nikin/Music/Aces High.mp3
+/home/nikin/Music/The Trooper.mp3
+/home/nikin/Music/Wasted Years.mp3
+/home/nikin/Music/Dirt Off Your Shoulder & Lying from You.mp3
+/home/nikin/Music/Big Pimpin & Papercut.mp3
+/home/nikin/Music/Jigga What & Faint.mp3
+/home/nikin/Music/Numb & Encore.mp3
+/home/nikin/Music/Izzo & In The End.mp3
+/home/nikin/Music/Points of Authority, 99 Problems & One Step Closer.mp3
 /home/nikin/Music/basantaa.mp3
+/home/nikin/Music/Mirrors.mp3
+/home/nikin/Music/Cry Me A River.mp3
+/home/nikin/Music/What Goes Around Comes Around.mp3
+/home/nikin/Music/Dark Horse.mp3
+/home/nikin/Music/E.T.mp3
+/home/nikin/Music/I Was Made For Lovin' You.mp3
+/home/nikin/Music/I Love It Loud.mp3
+/home/nikin/Music/Hard Luck Woman.mp3
+/home/nikin/Music/Heaven's On Fire.mp3
+/home/nikin/Music/Detroit Rock City.mp3
 /home/nikin/Music/Stairway To Heaven.mp3
-/home/nikin/Music/The Messenger.mp3
-/home/nikin/Music/Waiting For The End.mp3
+/home/nikin/Music/Whole Lotta Love.mp3
+/home/nikin/Music/Since I've Been Lovin' You.mp3
+/home/nikin/Music/Immigrant Song.mp3
+/home/nikin/Music/Babe I'm Gonna Leave You.mp3
+/home/nikin/Music/New Divide.mp3
+/home/nikin/Music/A Light That Never Comes.mp3
+/home/nikin/Music/Chance of Rain.mp3
 /home/nikin/Music/Papercut.mp3
 /home/nikin/Music/One Step Closer.mp3
 /home/nikin/Music/With You.mp3
-/home/nikin/Music/Points of Authority .mp3
+/home/nikin/Music/Points of Authority.mp3
 /home/nikin/Music/Crawling.mp3
 /home/nikin/Music/Runaway.mp3
 /home/nikin/Music/By Myself.mp3
-/home/nikin/Music/In the End .mp3
-/home/nikin/Music/A Place For My Head .mp3
+/home/nikin/Music/In the End.mp3
+/home/nikin/Music/A Place For My Head.mp3
 /home/nikin/Music/Forgotten.mp3
 /home/nikin/Music/Cure for the Itch.mp3
 /home/nikin/Music/Pushing Me Away.mp3
+/home/nikin/Music/My December.mp3
+/home/nikin/Music/High Voltage.mp3
 /home/nikin/Music/Foreword .mp3
 /home/nikin/Music/Don't Stay.mp3
 /home/nikin/Music/Somewhere I Belong.mp3
@@ -94,10 +195,10 @@
 /home/nikin/Music/Nobody's Listening.mp3
 /home/nikin/Music/Session.mp3
 /home/nikin/Music/Numb.mp3
-/home/nikin/Music/Wake .mp3
+/home/nikin/Music/Wake.mp3
 /home/nikin/Music/Given Up.mp3
 /home/nikin/Music/Leave Out All the Rest.mp3
-/home/nikin/Music/Bleed It Out .mp3
+/home/nikin/Music/Bleed It Out.mp3
 /home/nikin/Music/Shadow of the Day.mp3
 /home/nikin/Music/What I've Done.mp3
 /home/nikin/Music/Hands Held High.mp3
@@ -106,23 +207,52 @@
 /home/nikin/Music/In Between.mp3
 /home/nikin/Music/In Pieces.mp3
 /home/nikin/Music/The Little Things Give You Away.mp3
+/home/nikin/Music/The Requiem.mp3
+/home/nikin/Music/The Radiance.mp3
+/home/nikin/Music/Burning In The Skies.mp3
+/home/nikin/Music/Empty Spaces.mp3
+/home/nikin/Music/When They Come For Me.mp3
+/home/nikin/Music/Robot Boy.mp3
+/home/nikin/Music/Jornada Del Muerto.mp3
+/home/nikin/Music/Waiting For The End.mp3
+/home/nikin/Music/Blackout.mp3
+/home/nikin/Music/Wretches And Kings.mp3
+/home/nikin/Music/Wisdom, Justice, And Love.mp3
+/home/nikin/Music/Iridescent.mp3
+/home/nikin/Music/Fallout.mp3
+/home/nikin/Music/The Catalyst.mp3
+/home/nikin/Music/The Messenger.mp3
+/home/nikin/Music/Blackbirds.mp3
 /home/nikin/Music/LOST IN THE ECHO.mp3
 /home/nikin/Music/IN MY REMAINS.mp3
 /home/nikin/Music/BURN IT DOWN.mp3
 /home/nikin/Music/LIES GREED MISERY.mp3
-/home/nikin/Music/I'LL BE GONE .mp3
+/home/nikin/Music/I'LL BE GONE.mp3
 /home/nikin/Music/Castle Of Glass.mp3
-/home/nikin/Music/VICTIMIZED .mp3
+/home/nikin/Music/VICTIMIZED.mp3
 /home/nikin/Music/ROADS UNTRAVELED.mp3
 /home/nikin/Music/SKIN TO BONE.mp3
 /home/nikin/Music/UNTIL IT BREAKS.mp3
 /home/nikin/Music/TINFOIL.mp3
 /home/nikin/Music/POWERLESS.mp3
+/home/nikin/Music/Nobody Can Save Me.mp3
+/home/nikin/Music/Good Goodbye.mp3
+/home/nikin/Music/Talking to Myself.mp3
 /home/nikin/Music/Battle Symphony.mp3
+/home/nikin/Music/Invisible.mp3
 /home/nikin/Music/Heavy.mp3
+/home/nikin/Music/Sorry For Now.mp3
+/home/nikin/Music/Halfway Right.mp3
+/home/nikin/Music/One More Light.mp3
+/home/nikin/Music/Sharp Edges.mp3
 /home/nikin/Music/She Will Be Loved.mp3
+/home/nikin/Music/One More Night.mp3
 /home/nikin/Music/Payphone.mp3
 /home/nikin/Music/Memories.mp3
+/home/nikin/Music/Sugar.mp3
+/home/nikin/Music/Scared To Be Lonely.mp3
+/home/nikin/Music/Scared To Be Lonely (Acoustic).mp3
+/home/nikin/Music/In The Name of Love.mp3
 /home/nikin/Music/Fade to Black.mp3
 /home/nikin/Music/Master of Puppets.mp3
 /home/nikin/Music/Nothing Else Matters.mp3
@@ -130,6 +260,12 @@
 /home/nikin/Music/The Day That Never Comes.mp3
 /home/nikin/Music/The Unforgiven II.mp3
 /home/nikin/Music/The Unforgiven.mp3
+/home/nikin/Music/The Unforgiven III.mp3
+/home/nikin/Music/For Whom The Bell Tolls.mp3
+/home/nikin/Music/Sad But True.mp3
+/home/nikin/Music/Seek and Destroy.mp3
+/home/nikin/Music/Whiskey In The Jar.mp3
+/home/nikin/Music/Creeping Death.mp3
 /home/nikin/Music/Another Life.mp3
 /home/nikin/Music/Voices.mp3
 /home/nikin/Music/Disguise.mp3
@@ -139,12 +275,60 @@
 /home/nikin/Music/Thoughts & Prayers.mp3
 /home/nikin/Music/Welcome To The Black Parade.mp3
 /home/nikin/Music/I Don't Love You.mp3
+/home/nikin/Music/Famous Last Words.mp3
+/home/nikin/Music/Teenagers.mp3
+/home/nikin/Music/The Ghost of You.mp3
+/home/nikin/Music/Na Na Na.mp3
+/home/nikin/Music/Yo Jindagani.mp3
+/home/nikin/Music/Resham.mp3
+/home/nikin/Music/Bheda Ko Un Jasto.mp3
 /home/nikin/Music/Come As You Are.mp3
+/home/nikin/Music/Wonderwall.mp3
+/home/nikin/Music/Don't Look Back In Anger.mp3
+/home/nikin/Music/Aparichit Bhaawanna.mp3
+/home/nikin/Music/Timro nai maya.mp3
+/home/nikin/Music/Ek Dui Teen.mp3
+/home/nikin/Music/Fakaula Timlai.mp3
+/home/nikin/Music/Jaani na Jaani.mp3
+/home/nikin/Music/Juni Vari Lai (Acoustic).mp3
+/home/nikin/Music/Kalpana.mp3
+/home/nikin/Music/Khel Chakrai.mp3
+/home/nikin/Music/Kura.mp3
+/home/nikin/Music/Sunna.mp3
+/home/nikin/Music/Tibra Chahana.mp3
+/home/nikin/Music/Timi ra Ma.mp3
 /home/nikin/Music/Wish You Were Here.mp3
 /home/nikin/Music/Shine On You Crazy Diamond.mp3
 /home/nikin/Music/Comfortably Numb.mp3
 /home/nikin/Music/Coming Back To Life.mp3
+/home/nikin/Music/Another Brick In The Wall (Part 1).mp3
+/home/nikin/Music/Another Brick In The Wall (Part 2).mp3
+/home/nikin/Music/Have A Cigar.mp3
+/home/nikin/Music/Breathe.mp3
+/home/nikin/Music/Hey You.mp3
+/home/nikin/Music/Us and Them.mp3
+/home/nikin/Music/Money.mp3
+/home/nikin/Music/High Hopes.mp3
+/home/nikin/Music/Time.mp3
 /home/nikin/Music/Bohemian Rhapsody.mp3
+/home/nikin/Music/Deutschland.mp3
+/home/nikin/Music/Engel.mp3
+/home/nikin/Music/Sonne.mp3
+/home/nikin/Music/Amerika.mp3
+/home/nikin/Music/Ich Tu Dir Weh.mp3
+/home/nikin/Music/Ich Will.mp3
+/home/nikin/Music/Mein Herz Brennt.mp3
+/home/nikin/Music/Du Hast.mp3
+/home/nikin/Music/Feuer Frei!.mp3
+/home/nikin/Music/Reise, Reise.mp3
+/home/nikin/Music/Californication.mp3
+/home/nikin/Music/Can't Stop.mp3
+/home/nikin/Music/Dark Necessities.mp3
+/home/nikin/Music/Otherside.mp3
+/home/nikin/Music/Scar Tissue.mp3
+/home/nikin/Music/Snow.mp3
+/home/nikin/Music/Under The Bridge.mp3
+/home/nikin/Music/I'm Not The Only ONe.mp3
 /home/nikin/Music/Perfect.mp3
 /home/nikin/Music/Your Love Is A Lie.mp3
 /home/nikin/Music/When I'm Gone.mp3
@@ -188,11 +372,8 @@
 /home/nikin/Music/P.S. I Hate You.mp3
 /home/nikin/Music/Problem Child.mp3
 /home/nikin/Music/I Dream About You.mp3
-/home/nikin/Music/People=Sh#t.mp3
-/home/nikin/Music/Disasterpiece.mp3
-/home/nikin/Music/The Heretic Anthem.mp3
-/home/nikin/Music/Gently.mp3
-/home/nikin/Music/Left Behind.mp3
+/home/nikin/Music/All Out Life.mp3
+/home/nikin/Music/The Chapeltown Rag.mp3
 /home/nikin/Music/742617000027.mp3
 /home/nikin/Music/Sic.mp3
 /home/nikin/Music/Eyeless.mp3
@@ -202,15 +383,29 @@
 /home/nikin/Music/Tattered & Torn.mp3
 /home/nikin/Music/Frail Limb Nursery.mp3
 /home/nikin/Music/Purity.mp3
-/home/nikin/Music/ Liberate .mp3
+/home/nikin/Music/Liberate.mp3
 /home/nikin/Music/Prosthetics.mp3
 /home/nikin/Music/No Life.mp3
 /home/nikin/Music/Diluted.mp3
 /home/nikin/Music/Only One.mp3
 /home/nikin/Music/Scissors.mp3
-/home/nikin/Music/Eeyore .mp3
+/home/nikin/Music/Eeyore.mp3
 /home/nikin/Music/Me Inside.mp3
 /home/nikin/Music/Get This.mp3
+/home/nikin/Music/(515).mp3
+/home/nikin/Music/People=Sh#t.mp3
+/home/nikin/Music/Disasterpiece.mp3
+/home/nikin/Music/My Plague.mp3
+/home/nikin/Music/Everything Ends.mp3
+/home/nikin/Music/The Heretic Anthem.mp3
+/home/nikin/Music/Gently.mp3
+/home/nikin/Music/Left Behind.mp3
+/home/nikin/Music/The Shape.mp3
+/home/nikin/Music/I Am Hated.mp3
+/home/nikin/Music/Skin Ticket.mp3
+/home/nikin/Music/New Abortion.mp3
+/home/nikin/Music/Metabolic.mp3
+/home/nikin/Music/Iowa.mp3
 /home/nikin/Music/Prelude 3.0.mp3
 /home/nikin/Music/The Blister Exists.mp3
 /home/nikin/Music/Three Nil.mp3
@@ -270,6 +465,22 @@
 /home/nikin/Music/Solway Firth.mp3
 /home/nikin/Music/Zzyzx Rd..mp3
 /home/nikin/Music/Song #3.mp3
+/home/nikin/Music/Dying.mp3
+/home/nikin/Music/Imperfect.mp3
+/home/nikin/Music/Toxicity.mp3
+/home/nikin/Music/Chop Suey!.mp3
+/home/nikin/Music/Aerials.mp3
+/home/nikin/Music/Zombie.mp3
+/home/nikin/Music/Linger.mp3
+/home/nikin/Music/Nachaheko Hoina Timlai.mp3
+/home/nikin/Music/Kasari.mp3
+/home/nikin/Music/Thaha Chaina.mp3
+/home/nikin/Music/Mero Aanshu.mp3
+/home/nikin/Music/Samjhiney Mutu.mp3
+/home/nikin/Music/Yo dil Mero.mp3
+/home/nikin/Music/Maya.mp3
+/home/nikin/Music/Aaudai Jadai.mp3
+/home/nikin/Music/Parkhai.mp3
 /home/nikin/Music/Fear Inoculum.mp3
 /home/nikin/Music/Pneuma.mp3
 /home/nikin/Music/Ænema.mp3
@@ -304,6 +515,21 @@
 /home/nikin/Music/Intension.mp3
 /home/nikin/Music/Right In Two.mp3
 /home/nikin/Music/Viginti Tres.mp3
+/home/nikin/Music/Bhanai.mp3
+/home/nikin/Music/SAHARA.mp3
+/home/nikin/Music/Chinta.mp3
 /home/nikin/Music/Tear In My Heart.mp3
+/home/nikin/Music/Migrane.mp3
+/home/nikin/Music/Stressed Out.mp3
+/home/nikin/Music/Ride.mp3
+/home/nikin/Music/Trees.mp3
+/home/nikin/Music/Holding On To You.mp3
+/home/nikin/Music/Heathens.mp3
 /home/nikin/Music/With Or Without You.mp3
 /home/nikin/Music/Sleep Like a Baby Tonight.mp3
+/home/nikin/Music/Ordinary Love.mp3
+/home/nikin/Music/U2 One.mp3
+/home/nikin/Music/The Saints are Coming.mp3
+/home/nikin/Music/I'll Be Your Lover Too.mp3
+/home/nikin/Music/Brand New Day.mp3
+/home/nikin/Music/Days Like This.mp3
