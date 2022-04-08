e550b01af953bd7681170d75cb21011cf0ee137f
Nikin Baidar <nikinbaidarr@gmail.com>
2022-04-08 13:00:18 +0545

    synchronize dotfiles

--- config/dwm/config.h
+++ config/dwm/config.h
@@ -11,7 +11,7 @@ static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#bbbbbb";
 static const char col_gray3[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#005073";
 static const char *colors[][3]      = {
         /*               fg         bg         border   */
         [SchemeNorm] = { col_gray3, "#1d2021", "#b2be85" },
